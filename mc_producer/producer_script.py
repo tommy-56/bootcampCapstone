331d0c395a55e55a640411e5b8b808350ec04da0
Thomas Anderson <tommyandersonpersonal56@gmail.com>
2024-07-23 09:58:15 -0700

    Cleaned producer script up
    also fixed it from crashing when kafka isnt up yet

--- mc_producer/producer_script.py
+++ mc_producer/producer_script.py
@@ -1,14 +1,10 @@
 from kafka import KafkaProducer
+from kafka.errors import NoBrokersAvailable
 import time, os, docker
 
-print("Starting Script")
-# Kafka broker address
-bootstrap_servers = 'kafka-server:9092'
-time.sleep(5) #Waiting for Kafka to fully boot up
 
-# Initialize Kafka producer
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
+#Get logs from the minecraft container
 def fetch_minecraft_logs():
     print("Getting Container Logs")
     container_name = "bootcampcapstone-minecraft-1"
@@ -26,8 +22,7 @@ def fetch_minecraft_logs():
         return f"An error occurred: {e}"
 
 
-#Has More information
-#Everytime MC reboots it zips the log file and we don't get the data about the reboot from the latest.log file
+# Via the Kafka Producer send logs to kafka topic
 def send_logs_to_kafka(log_message):
     try:
         producer.send('minecraft_new_logs_topic', log_message.encode('utf-8'))
@@ -35,16 +30,30 @@ def send_logs_to_kafka(log_message):
     except Exception as e:
         print(f'Error Occured Sending Container Logs to Kafka will retry: {e}')
 
-# Example usage
-while True:
-    # Fetch Minecraft logs
-    minecraft_logs = fetch_minecraft_logs()
-    # Send logs to Kafka if logs are available
-    if minecraft_logs:
-        send_logs_to_kafka(minecraft_logs)
-    
-    # Simulate interval between log messages
-    time.sleep(30)  # Adjust interval as needed
 
-# Close Kafka producer (Note: This won't be reached in the current script as it's in an infinite loop)
-producer.close()
+
+if __name__ == '__main__':
+    print("Starting Script")
+    kafka_healthy = False
+    while kafka_healthy == False:
+        try:
+            # Kafka broker address
+            bootstrap_servers = 'kafka-server:9092'
+            # Initialize Kafka producer
+            producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+            kafka_healthy = True
+        except NoBrokersAvailable as e:
+            print(f"Error: {e}")
+            time.sleep(5)
+    
+    while True:
+        print("Fetching Logs")
+        # Fetch Minecraft logs
+        minecraft_logs = fetch_minecraft_logs()
+        # Send logs to Kafka if logs are available
+        if minecraft_logs:
+            send_logs_to_kafka(minecraft_logs)
+        
+        # Send Updates on an interval to keep kafka updated
+        time.sleep(30)  # Adjust interval as needed
+
